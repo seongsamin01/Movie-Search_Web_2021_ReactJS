317629d248ff11cf374a46aef4ecb4c93fa2ac95
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-24 16:07:59 +0900

    homecontainer update
    에러계속 떠서 어디가 원인인지 못찾아서 소스코드보면서 update

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -2,47 +2,49 @@ import React from "react";
 import HomePresenter from "./HomePresenter";
 import { moviesApi } from "api";
 
-
 export default class extends React.Component {
-    state = {
-        nowPlaying: null,
-        upcoming: null,
-        popular: null,
-        error: null,
-        loading: true
-    };
+  state = {
+    nowPlaying: null,
+    loading: true
+  };
 
-    async componentDidMount() {
-        try {
-          const { data: { results: nowPlaying }} = await moviesApi.nowPlaying();
-          const { data: { results: upcoming }} = await moviesApi.upcoming();
-          const { data: { results: popular}} = await moviesApi.popular();
-          this.setState({
-              nowPlaying,
-              upcoming,
-              popular
-          });
-        } catch {
-          this.setState({
-              error: "Can't find movie information."
-          });
-        } finally {
-          this.setState({
-              loading: false
-          });
-        }
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upcoming }
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular }
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
     }
+  }
 
-    render() {
-        const { nowPlaying, upcoming, popular, error, loading } = this.state;
-        return (
-            <HomePresenter
-                nowplaying={nowPlaying}
-                upcoming={upcoming}
-                popular={popular}
-                error={error}
-                loading={loading}
-            />    
-        );
-    }
+  render() {
+    const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upcoming={upcoming}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
 }
