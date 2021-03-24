b0bd7c4ac61c10c9c6a27b13764452e40664a40c
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-24 15:38:33 +0900

    detail container # mount 하기
    const에 parsedId 넣어주기.
    url 숫자-> 해당 id detail 이동
    url 문자-> home으로 리턴
    
    const parsedId = parseInt(id);
          if (isNaN(parsedId)) {
             return push("/");

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,14 +8,22 @@ export default class extends React.Component {
         loading: true
     };
 
+    async componentDidMount() {
+        const { 
+          match: {
+            params: { id }
+        },
+        history: { push }
+      } = this.props;
+      const parsedId = parseInt(id);
+      if (isNaN(parsedId)) {
+         return push("/");
+     } 
+  }
+
     render() {
         const { result, error, loading } = this.state;
         return (
-            <DetailPresenter
-                result={result}
-                error={error}
-                loading={loading}
-            />    
-        );
+            <DetailPresenter result={result} error={error} loading={loading} /> );
     }
 }
