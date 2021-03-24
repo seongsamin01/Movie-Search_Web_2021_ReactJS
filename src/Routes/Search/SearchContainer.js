a56279d02c4573b325203680f53a65f55335558b
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-24 14:38:11 +0900

    search container# mount하기
    async, await 사용
    componentDidMount안에
    try, catch, finally 함수 사용
    console에서 results항목만 지정해서 보여주기 const { data: { results: nowPlaying }}
    ========================
    tv & movies의 detail url주소 만들기
    <Route path="/movie/:id" component={Detail} />
            <Route path="/show/:id" component={Detail} />

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { moviesApi, tvApi } from "api";
+import React from "react"; 
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component {
@@ -10,8 +11,41 @@ export default class extends React.Component {
         error: null
     };
 
+    componentDidMount() {
+        this.handlSubmit();
+    }
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if (searchTerm !== "") {
+            this.searchByTerm();
+        }
+    };
+
+    searchByTerm = async () => {
+        const { searchTerm } = this.state;
+        this.setState({ loading: true});
+        try {
+          const { 
+            data: { results: movieResults }
+          } = await moviesApi.search(searchTerm);
+          const { 
+            data: { results: tvResults }
+          } = await tvApi.search(searchTerm);
+        this.setState({
+            movieResults,
+            tvResults
+          });
+        } catch {
+          this.setState({ error: "Can't find results. "});
+        } finally {
+          this.setState({ loading: false });
+        }
+    };
+
     render() {
         const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+        console.log(this.state);
         return (
         <SearchPresenter
            movieResults={movieResults}
@@ -19,7 +53,8 @@ export default class extends React.Component {
            searchTerm={searchTerm}
            loading={loading}
            error={error} 
-            />
-        );
-    }
+           handleSubmit={this.handleSubmit}
+        />
+      );
+   }
 }
