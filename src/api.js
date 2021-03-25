7057dec56bb63be8433aa7af7b8b1ce90f60c23b
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-25 11:51:16 +0900

    tv/search container myself
    tv container console 해결함.
    How? Detail, Search, TV라는 불필요한 파일이 있었던거 같아서 그걸 삭제했더니 에러 떠서 다시 복구하니까 콘솔이 떴음!!! (nico는 없었음. 그리고 home container는 콘솔 잘됐는데, home.js는 또 없었음)

--- src/api.js
+++ src/api.js
@@ -33,7 +33,8 @@ export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
     airingToday: () => api.get("tv/airing_today"), 
-    showDetail: id => api.get(`tv/${id}`, {
+    showDetail: id =>
+     api.get(`tv/${id}`, {
         params: {
             append_to_response: "videos"
 
