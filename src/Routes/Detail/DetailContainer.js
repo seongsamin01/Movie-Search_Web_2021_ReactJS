6ee4970bdf48c0d3ac91c40987fc3cd50c08550a
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-25 13:11:12 +0900

    detail container # mount하기 pt2
    localhost:3001/movie/123423
    movie-> pathname
    123423->push
    
     try {
              if (isMovie) {
                  const request = await moviesApi.movieDetail(parsedId);
                  result = request.data;
              } else {
                  const request = await tvApi.showDetail(parsedId);
                  result = request.data;
              }
              console.log(result);
    
    만약 movie url요청 -> movieApi호출
    그게 아니라면 ->tvApi호출

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,23 +1,46 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-    state = {
+    constructor(props) {
+      super(props);
+      const {location: { pathname }} = props;
+      this.state = {
         result: null,
         error: null,
-        loading: true
+        loading: true,
+        isMovie: pathname.includes("/movie/")
     };
+}  
 
     async componentDidMount() {
         const { match: { params: { id }}, history: { push }} = this.props;
+        const { isMovie } = this.state;
         const parsedId = parseInt(id);
         if (isNaN(parsedId)) {
             return push("/");
+        }
+        let result = null;
+        try {
+          if (isMovie) {
+              const request = await moviesApi.movieDetail(parsedId);
+              result = request.data;
+          } else {
+              const request = await tvApi.showDetail(parsedId);
+              result = request.data;
+          }
+          console.log(result);
+        } catch {
+            this.setState({ error: "Can't find anything! ~_~;;;;;;;" });
+        } finally {
+            this.setState({ loading: false, result });
         }
     }
 
     render() {
         const {result, error, loading } = this.state;
+        console.log(this.state);
         return <DetailPresenter result={result} error={error} loading={loading} />     
     }
 }
