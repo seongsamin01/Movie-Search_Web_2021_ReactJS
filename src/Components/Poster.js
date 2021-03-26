7979dd059d59d8bcf4b439f995d338c12d0fb6a1
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-26 14:44:41 +0900

    poster component 스타일링하기
    poster component 스타일링 해주기( css)
    
    사진첨부해서 넣어주기 src assets폴더에
    
    movie or tv show title 18컷으로 잘라주기 (substring)

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -4,24 +4,62 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 
-const Container = styled.div``;
 
-const ImageContainer = styled.div``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
-const Image = styled.div``;
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl});
+    height: 180px;
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+const Rating = styled.span`
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+`;
 
-const Title = styled.span``;
+const ImageContainer = styled.div`
+    margin-bottom: 5px;
+    position: relative;
+    &:hover {
+        ${Image} { 
+           opacity: 0.3; 
+        }
+        ${Rating} {
+            opacity: 1;
+        }
+    }
+`;
 
-const Year = styled.span``;
+const Title = styled.span`
+    display: block;
+    margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 
 const Poster = ({id, imageUrl, title, rating, year, isMovie = false }) => (
     < Link to ={isMovie ? `/movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl}/>
+                <Image bgUrl={
+                    imageUrl 
+                        ? `https://image.tmdb.org/t/p/w300${imageUrl}` 
+                        : require("../assets/noPosterSmall.png").default 
+                    }
+                />
                 <Rating>
                 <span role="img" aria-label="rating">
                     ⭐️
@@ -29,7 +67,9 @@ const Poster = ({id, imageUrl, title, rating, year, isMovie = false }) => (
                 {" "}{rating}/10
                 </Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>
+                {title.length > 18 ? `${title.substring(0, 15)}...` : title}
+            </Title>
             <Year>{year}</Year>
         </Container>
     </Link>
