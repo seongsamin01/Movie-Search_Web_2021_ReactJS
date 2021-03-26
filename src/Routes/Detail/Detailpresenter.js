2a1afc56591b93dff1014bbf2b4b7822ae24eb33
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-26 17:17:39 +0900

    detail container 만들기 #2
    이 앞전에께 detail container였는데 poster container로 제목을 잘못 적었다.
    
    여기서는
    영화 설명란에
    Data
    Title
    ItemContainer
    Item
    Divider
    Overview
    각각을 css로 스타일링 해주고,
    내용을 삽입해주었다.
    
    물론 명칭은 movie, tvshow에 따라 각 확인해주며 정해줬다.
    ex. movie- runtime
         tv - episode_run_time

--- src/Routes/Detail/Detailpresenter.js
+++ src/Routes/Detail/Detailpresenter.js
@@ -19,7 +19,7 @@ const Backdrop = styled.div`
     background-image: url(${props => props.bgImage});
     background-position: center center;
     background-size: cover;
-    filter: blur(3px);
+    filter: blur(8px);
     opacity: 0.5;
     z-index: 0;
 `;
@@ -41,6 +41,38 @@ const Cover = styled.div`
     border-radius: 5px;
 `;
 
+const Data = styled.div`
+    width: 70%;
+    margin-left: 10%;
+`;
+
+const Title = styled.h3`
+    font-size: 38px;
+    margin-bottom: 10px;
+`;
+
+const ItemContainer = styled.div`
+    margin: 20px 20px;
+    color: #81ecec;
+`;
+
+const Item = styled.span`
+    font-size: 18px;
+    opacity: 0.7;
+`;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 18px;
+    opacity: 0.9;
+    line-height: 1.8;
+    width: 80%;
+`;
+
+
 const DetailPresenter = ({ result, loading, error }) => (
     loading ? (
         <Loader />
@@ -56,8 +88,36 @@ const DetailPresenter = ({ result, loading, error }) => (
                      ? `https://image.tmdb.org/t/p/original${result.poster_path}` 
                      : require("../../assets/noPosterSmall.png")
                 }
-              /> 
-          </Content>
+              />
+              <Data>
+                 <Title>
+                     {result.original_title 
+                        ? result.original_title 
+                        : result.original_name }
+                 </Title>
+                 <ItemContainer>
+                    <Item>
+                       {result.release_date 
+                          ? result.release_date.substring(0, 4)
+                          : result.first_air_date.substring(0, 4)}
+                    </Item>
+                    <Divider>◦</Divider>
+                    <Item>
+                        {result.runtime ? result.runtime : result.episode_run_time[0]} min
+                    </Item>
+                    <Divider>◦</Divider>
+                    <Item>
+                        {result.genres &&
+                           result.genres.map((genre, index) =>
+                             index === result.genres.length - 1
+                               ? genre.name
+                               : `${genre.name} / `
+                        )}
+                    </Item>
+                 </ItemContainer>
+                 <Overview>{result.overview}</Overview> 
+              </Data> 
+          </Content>                
         </Container>)
 );
 
